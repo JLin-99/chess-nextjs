3d421b274df11fd15e6c108681d71653b056fcd0
José Lin <lin-jose@outlook.com>
2022-10-18 20:25:38 -0200

    Create Login Component

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
+import styles from "../styles/Login.module.css";
 
 export default function Login({ setUsername }) {
   const [user, setUser] = useState("");
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setUsername(user);
+  };
+
   return (
-    <div>
-      <form onSubmit={() => setUsername(user)}>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-        />
-        <input type="submit" value="Go!" />
-      </form>
+    <div className={styles.popup}>
+      <div className={styles.popupInner}>
+        <h1>Welcome to ChocoChess!!!</h1>
+        <div className={styles.username}>
+          <label>Choose your username:</label>
+          <input
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <div className={styles.options}>
+          <button onClick={handleOnSubmit}>Create New Game!</button>
+          <button>Join Game!</button>
+        </div>
+      </div>
     </div>
   );
 }
--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import spinner from "../public/assets/spinner.gif";
+import styles from "../styles/Spinner.module.css";
 
 export default function Spinner() {
   return (
-    <div style={{ position: "fixed" }}>
+    <div className={styles.spinner}>
       <Image src={spinner} />
     </div>
   );
